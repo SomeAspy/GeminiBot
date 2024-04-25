8f90d7a2ea0130a84b385ea4684f156035665ba9
Aiden <amb@aspy.dev>
2024-04-25 19:16:49 -0400

    Package Updates

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -4,10 +4,10 @@ import { geminiGenerate } from "../lib/gemini";
 import untypedConfig from "../../config/config.json" assert { type: "json" };
 import type { Config } from "../types/config.js";
 
-const {ChannelIDLock} = untypedConfig as Config;
+const {ChannelID} = untypedConfig as Config;
 
 export async function handleMessage(message:Message){
-    if(!message.inGuild() || message.channelId!=ChannelIDLock){
+    if(!message.inGuild() || message.channelId!=ChannelID){
         return;
     }
     await geminiGenerate(message)
